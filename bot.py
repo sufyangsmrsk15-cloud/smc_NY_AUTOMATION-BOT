b0897557d9098ba9107d2a05d924bc5a537a7035
sufyangsmrsk15-cloud <sufyangsmrsk15@gmail.com>
2025-10-31 11:56:42 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,6 +73,6 @@ threading.Thread(target=scheduler_thread).start()
 
 import os
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
